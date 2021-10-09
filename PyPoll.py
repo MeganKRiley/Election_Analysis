bb2aa9fee952bf4fc328014f6ff1f1f9e1558bf5
Megan Riley <90050622+MeganKRiley@users.noreply.github.com>
2021-10-09 14:53:48 -0500

    Updated file with candidates name

--- PyPoll.py
+++ PyPoll.py
@@ -26,19 +26,31 @@ with open(file_to_save, "w") as txt_file:
      # Write three counties to the file.
      txt_file.write("Arapahoe\nDenver\nJefferson")
 
+# 1. Initialize a total vote counter. 
+total_votes = 0
+
+# Open the election results and read the file
+candidate_options = []
+     
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)   
 
-    #To do: read and analyze data here. 
-    # Read the file object with the reader function.
-    file_reader = csv.reader(election_data)
-
     # Read and print the header row.
     headers = next(file_reader)
-    print(headers)
-
+    
     # Print each row in the CSV file.
     for row in file_reader:
-        print(row)
+        #2. Add to the total vote count. 
+        total_votes += 1
 
+        # Print the candidate name from each row.
+        candidate_name = row[2]
+
+        # If the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+            # Add it to the list of candidates.
+            candidate_options.append(candidate_name)
+
+    # Print the candidate list.
+    print(candidate_options)
